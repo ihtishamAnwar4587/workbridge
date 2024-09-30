2d8b1f948ec5278b52d8a988767968c720232e93
Muhammad Ihtisham Anwar <ihtishama@workbridgeteam.ca>
2024-09-30 14:05:31 +0500

    web.php has been modified

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/hell', static function() {
+    dd('here the hell you are');
+});
+
 require __DIR__.'/auth.php';
