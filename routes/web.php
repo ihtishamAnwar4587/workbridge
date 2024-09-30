4790c2786bd343a8e224e5dee2863c96fcf405df
Muhammad Ihtisham <muhammadihtisham@Muhammads-MacBook-Pro.local>
2024-09-30 08:51:25 +0500

    new another route has been added

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::get('/another-route', static function() {
+    return "another route has been added successfully!!";
+});
+
 require __DIR__.'/auth.php';
