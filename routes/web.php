436c974545bf9245ee7160851a822e9f3f3bd67b
Muhammad Ihtisham Anwar <ihtishama@workbridgeteam.ca>
2024-09-30 14:01:44 +0500

    hello route has been removed successfully from web.php

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/hello', static function() {
-    dd('new route has been added');
-});
-
 require __DIR__.'/auth.php';
