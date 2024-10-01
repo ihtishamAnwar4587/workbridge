46c3a1f0a3da647e5c3c05570ba68f0c12554c3d
Muhammad Ihtisham Anwar <ihtishama@workbridgeteam.ca>
2024-10-01 11:36:58 +0500

    web.php hell is here

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/hell', static function() {
+    dd('welcome to hell');
+});
+
 require __DIR__.'/auth.php';
